ec98554aec48252caf24edbb22ce63e7ee24dc60
Grazi998 <grazianokaluza@gmail.com>
2023-01-13 13:32:11 +0100

    wip 13012023

---
+++ EKupi.WebApi/Controllers/UserAccountController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EKupi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserAccountController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserAccountController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> Register()
+
+        {
+
+        }
+    }
+}
